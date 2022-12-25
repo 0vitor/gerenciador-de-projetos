7db8f2dfcbba7e16f5a8393126f1ea8ec73398a8
0vitor <w.wutaes@gmail.com>
2022-12-25 10:26:29 -0300

    chore: add mongoose

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
 import express from 'express'
 import colaboratorRouter from "./Router/collaborator.js"
+import mongoose from 'mongoose'
+
+const url = "mongodb+srv://bancoTDD:n303qDfGy6VWnrjd@demo.qiebebi.mongodb.net/?retryWrites=true&w=majority"
+mongoose.set('strictQuery', true)
+mongoose.connect(url)
+
+mongoose.connection.on('connected', () => {
+  console.log('conectado')
+})
 
 const app = express()
+app.use(express.json())
 
 app.use("/colaboradores", colaboratorRouter)
 
