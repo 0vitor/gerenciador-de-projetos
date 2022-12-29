ca1d2ea85cfeb5c01171a93ac37019f1fefb4c1d
0vitor <w.wutaes@gmail.com>
2022-12-29 14:30:31 -0300

    fix: fixing type of foreing key

--- src/model/project.js
+++ src/model/project.js
@@ -1,14 +1,12 @@
 import mongoose from "mongoose";
-
 const Schema = mongoose.Schema
-
 const projectSchema = new Schema({
-  name: {type: String, required: true},
+  name: {type: String, required: true, unique: true, trim: true},
   description: {type: String, required: true},
   status: {type: String, required: true },
-  value: {type: String, required: true},
-  calloborators: {type: [Schema.ObjectId]},
-  companies: {type: [Schema.ObjectId]}
+  value: {type: Number, required: true},
+  collaborators: [{type: Schema.Types.ObjectId, ref: 'collaborator'}],
+  companies: [{type: Schema.Types.ObjectId, ref: 'company'}]
 })
 
 const Project = mongoose.model('project', projectSchema)
