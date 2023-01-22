2c31b9ca79d2fd9df33f03314cd2e1d75000864b
0vitor <w.wutaes@gmail.com>
2023-01-22 19:47:55 -0300

    feat: add handle error and input validator

--- src/controller/collaborator-controller.js
+++ src/controller/collaborator-controller.js
@@ -1,39 +1,65 @@
 import Collaborator from "../model/collaborator.js"
-import verifyPassowrd from "../infra/cryptography/verify-passowrd.js"
-import collaboratorQuery from "../infra/mongodb/collaborator-query.js"
+import encrypt from "../infra/cryptography/encrypt.js"
+import yup from "yup"
+import errorHandler from "../error/error-handle.js"
+
+const schemaCollaborator = yup.object().shape({
+  name: yup.string().required('Erro: Necessário preencher o campo nome!'),
+  email: yup.string()
+    .email('Erro: Email com formato inválido')
+    .required('Erro: Necessário preencher o campo email!'),
+  passowrd: yup.string()
+    .min(8, 'Erro: Necessário ter no mínimo 8 caracteres!')
+    .required('Erro: Necessário ter no mínimo 8 caracteres!'),
+  role: yup.string().required('Erro: Necessário preencher o campo função!')
+})
 
 const getAll = async (req, res) => {
   const response = await Collaborator.find({})
   res.send(response)
 }
 
-const getOne = async (req, res) => {
-  const { email } = req.body
-  const response = await Collaborator.findOne({ email })
-  res.send(response)
+const getEmail = async (req, res) => {
+  try {
+    console.log(req)
+    const { email } = req.query
+    const response = await Collaborator.findOne({ email })
+    if (response)
+      res.send(response)
+    else
+      res.status(404).send('not found')
+  } catch (err) {
+    res.status(400).send('error')
+  }
 }
 
 const save = async (req, res) => {
-  const result = await collaboratorQuery.create(req.body)
-  const emailAlreadyExist = 11000
-  if (result == emailAlreadyExist) {
-    res.send('email ja existe')
-  } else if (!result) {
-    res.status(400).send('ocorreu um erro')
-  } else {
-    res.send('registrado com sucesso')
+  try {
+    const collaborator = req.body
+    await schemaCollaborator.validate(collaborator)
+    collaborator.passowrd = encrypt(collaborator.passowrd);
+    await Collaborator.create(collaborator)
+    return res.status(201).send('criado com sucesso!')
+  } catch (err) {
+    errorHandler(res, err, 'email')
   }
 }
 
 const update = async (req, res) => {
-  const response = await collaboratorQuery.updateOne(req.body)
-  const emailAlreadyExist = 11000
-  if (response == emailAlreadyExist)
-    return res.send('email ja existe')
-  if (!response)
-    return res.status(400).send('ocorreu um erro')
-  if (response)
-    return res.send(response)
+  try {
+    const collaborator = req.body
+    const { email } = req.query
+    await schemaCollaborator.validate(collaborator)
+    collaborator.passowrd = encrypt(collaborator.passowrd)
+    const result = await Collaborator.updateOne({ email }, collaborator)
+    if (result.modifiedCount)
+      return res.send('modificado com sucesso!')
+    else
+      return res.status(400).send('ocorreu um erro')
+
+  } catch (err) {
+    errorHandler(res, err, 'email')
+  }
 }
 
 const deleteAll = async (req, res) => {
@@ -41,16 +67,15 @@ const deleteAll = async (req, res) => {
   res.send(response)
 }
 
-const deleteOne = async (req, res) => {
-  const { email, passowrd } = req.body
-  const collaborator = await collaboratorQuery.findOneWithPassowrd(email)
-  const isValid = collaborator && verifyPassowrd(passowrd, collaborator.passowrd)
-  if (isValid) {
-    await collaboratorQuery.deleteOne(email)
+const deleteByEmail = async (req, res) => {
+  try {
+    const { email } = req.body
+    await Collaborator.deleteOne({ email })
     res.send('removido com sucesso')
-  } else {
+  } catch (err) {
+    console.log(err)
     return res.status(400).send('ocorreu um erro')
   }
 }
 
-export default { getAll, save, update, deleteAll, deleteOne, getOne }
+export default { getAll, getEmail, save, update, deleteAll, deleteByEmail }
