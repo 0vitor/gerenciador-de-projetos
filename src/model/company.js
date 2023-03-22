ac8d4f1874feb760ff875c4f35c37d34c0d35b5e
0vitor <w.wutaes@gmail.com>
2023-03-22 14:49:24 -0300

    refactor: change type to String

--- src/model/company.js
+++ src/model/company.js
@@ -3,8 +3,8 @@ const Schema = mongoose.Schema
 const companySchema = new Schema({
   name: {type: String, required: true, unique: true, trim: true},
   address: {type: String, required: true},
-  cnpj: {type: Number, required: true },
-  phoneNumber: {type: Number, required: true},
+  cnpj: {type: String, required: true },
+  phoneNumber: {type: String, required: true},
 })
 
 const Company = mongoose.model('company', companySchema)
