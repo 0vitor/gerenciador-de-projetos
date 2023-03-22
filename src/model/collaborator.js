672276b5ba8a6457e3f18ffc27a17bbdeda70da0
0vitor <w.wutaes@gmail.com>
2023-03-22 14:47:45 -0300

    refactor: change name attribute 'funcao' to 'role'

--- src/model/collaborator.js
+++ src/model/collaborator.js
@@ -6,7 +6,7 @@ const CollaboratorsSchema = new Schema({
   name: {type: String, required: true},
   email: {type: String, required: true, unique: true},
   passowrd: {type: String, required: true, select: false},
-  funcao: {type: String, required: true}
+  role: {type: String, required: true}
 })
 
 const Collaborator = mongoose.model("collaborator", CollaboratorsSchema)
