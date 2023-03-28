db722f05af4f36150a20e26e34f45986743944a5
0vitor <w.wutaes@gmail.com>
2023-03-28 09:27:43 -0300

    fix: change name of end-point

--- src/router/project.js
+++ src/router/project.js
@@ -5,10 +5,10 @@ import projectController from '../controller/project-controller.js'
 const router = express.Router()
 
 router.get('/', (req, res) => projectController.getAll(req, res))
-router.get('/one', (req, res) => projectController.getOne(req, res))
+router.get('/findByName', (req, res) => projectController.getOne(req, res))
 router.post('/', verifyToken, (req, res) => projectController.save(req, res))
 router.put('/', verifyToken, (req, res) => projectController.update(req, res))
 router.delete('/', verifyToken, (req, res) => projectController.deleteAll(req, res))
-router.delete('/one', verifyToken, (req, res) => projectController.deleteOne(req, res))
+router.delete('/findByName', verifyToken, (req, res) => projectController.deleteOne(req, res))
 
 export default router
